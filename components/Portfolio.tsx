89bb941742a2fd367c166052c041b8604841fbd5
Maxim Degtiarev <mxim.degt@gmail.com>
2025-04-29 13:55:14 -0300

    Production deployement fixes

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -5,55 +5,37 @@ import Image from "next/image";
 const Portfolio = () => {
   const works = [
     {
-      title: "StartMaxUp",
-      subtitle: "StartUp&apos;s Directory",
-      description: `A web application built on a RESTful API architecture to
-                  provide users with complete control over their personal
-                  finances. Includes: Automated generation of expense summaries,
-                  secure user authentication and email verification for enhanced
-                  data protection.`,
+      title: "Start-up computer conmpany",
+      subtitle: "Start-up website design and development",
+      description: `A modern computer requirements website for a startup, built 
+      with a Figma-designed interface and a robust Next.js/React framework 
+      for optimal performance and scalability.`,
       image:
-        "https://res.cloudinary.com/djdnlogf1/image/upload/v1737475216/ycdirectory_lwzasu.png",
+        "https://res.cloudinary.com/djdnlogf1/image/upload/v1745942260/Screenshot_2025-04-29_125508_mljyks.png",
       image_width: 600,
       image_height: 400,
-      image_alt: "YCDirectory Next.js Course",
-      link: "https://ycdirectory.vercel.app/",
-      tech: `Next.js, React, TypeScript, Lucide, Sanity, Next-Auth, Vercel,
-                  React Dom, Tailwind CSS, Figma, Jest, VS Code, Markdown, Git,
+      image_alt: "Computer conmpany website hero",
+      link: "https://jm99-website.vercel.app/",
+      tech: `Next.js, React, TypeScript, Lucide, Shadcn, Vercel,
+                  React Dom, Tailwind CSS, Figma, VS Code, Git,
                   Github`,
     },
     {
-      title: "Bancax",
-      subtitle: "StartUp&apos;s Directory",
-      description: `A web application built on a RESTful API architecture to
-                  provide users with complete control over their personal
-                  finances. Includes: Automated generation of expense summaries,
-                  secure user authentication and email verification for enhanced
-                  data protection.`,
+      title: "Appointment Booking System",
+      subtitle: "Client and Admin appointment administration",
+      description: `Designed and built an efficient appointment booking platform 
+      for both clients and administrators, leveraging Next.js for a smooth user 
+      experience, Redux for robust state management, and a secure PostgreSQL 
+      database on AWS RDS accessed via a RESTful API.`,
       image:
-        "https://res.cloudinary.com/djdnlogf1/image/upload/v1737475215/bancax_ybo6xo.png",
+        "https://res.cloudinary.com/djdnlogf1/image/upload/v1745942475/Screenshot_2025-04-29_130055_shybj5.png",
       image_width: 600,
       image_height: 400,
-      image_alt: "Bancax website By Maxim Degtiarev",
-      link: "https://bancax.vercel.app",
-      tech: `React, Node, Express, MongoDB, JWT, Mongoose, BCrypt,
-                  Nodemailer, Cloudinary, RESTfull API, Figma, Vite, Vercel,
-                  Jest, VS Code, Github`,
-    },
-    {
-      title: "Slack Clone",
-      subtitle: "Community chat app",
-      description: `Developed a real-time chat application using the MERN stack,
-                  featuring user authentication, direct messaging updates, group
-                  channels, and message persistence.`,
-      image:
-        "https://res.cloudinary.com/djdnlogf1/image/upload/v1737475216/slack_y5oila.png",
-      image_width: 600,
-      image_height: 400,
-      image_alt: "Slack Clone By Macks",
-      link: "https://slack-clone-macks-v2.vercel.app/",
-      tech: `React, Node, Express, MongoDB, JWT, Mongoose, BCrypt, RESTfull
-                  API, Figma, Vite, Vercel, VS Code, Jest, Firebase, Github`,
+      image_alt: "Booking form",
+      link: "https://maraxo-app.vercel.app/",
+      tech: `Next.js, React, TypeScript, AWS RDS, Shadcn, Vercel, Redux,
+            PostrgeSQL, React Query, Tailwind CSS, Figma, VS Code, Git,
+            Github, Zod, Jest`,
     },
     {
       title: "Natourex",
@@ -73,6 +55,39 @@ const Portfolio = () => {
                   Bcrypt, Helmet, PUG, Nodemailer, Slugify, Leaflet, MondoDB,
                   Mongoose, JWT, Netlify, Stripe, Babel, Cypress, RESTFull API,
                   Postman, VS Code, Github`,
+    },
+    // {
+    //   title: "Bancax",
+    //   subtitle: "StartUp&apos;s Directory",
+    //   description: `A web application built on a RESTful API architecture to
+    //               provide users with complete control over their personal
+    //               finances. Includes: Automated generation of expense summaries,
+    //               secure user authentication and email verification for enhanced
+    //               data protection.`,
+    //   image:
+    //     "https://res.cloudinary.com/djdnlogf1/image/upload/v1737475215/bancax_ybo6xo.png",
+    //   image_width: 600,
+    //   image_height: 400,
+    //   image_alt: "Bancax website By Maxim Degtiarev",
+    //   link: "https://bancax.vercel.app",
+    //   tech: `React, Node, Express, MongoDB, JWT, Mongoose, BCrypt,
+    //               Nodemailer, Cloudinary, RESTfull API, Figma, Vite, Vercel,
+    //               Jest, VS Code, Github`,
+    // },
+    {
+      title: "Slack Clone",
+      subtitle: "Community chat app",
+      description: `Developed a real-time chat application using the MERN stack,
+                  featuring user authentication, direct messaging updates, group
+                  channels, and message persistence.`,
+      image:
+        "https://res.cloudinary.com/djdnlogf1/image/upload/v1737475216/slack_y5oila.png",
+      image_width: 600,
+      image_height: 400,
+      image_alt: "Slack Clone By Macks",
+      link: "https://slack-clone-macks-v2.vercel.app/",
+      tech: `React, Node, Express, MongoDB, JWT, Mongoose, BCrypt, RESTfull
+                  API, Figma, Vite, Vercel, VS Code, Jest, Firebase, Github`,
     },
   ];
 
@@ -98,13 +113,20 @@ const Portfolio = () => {
                 className="flex md:space-x-6 flex-col md:flex-row object"
               >
                 <div className="md:w-1/2 h-auto md:h-[55%] outline rounded-sm outline-[.2rem] outline-offset-[.1rem] outline-blue-500 overflow-hidden hover:scale-125 ease-in-out duration-300">
-                  <Image
-                    className="w-full object-cover"
-                    src={work.image}
-                    alt={work.image_alt}
-                    width={work.image_width}
-                    height={work.image_height}
-                  />
+                  <Link
+                    href={work.link}
+                    className="cursor-pointer"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <Image
+                      className="w-full object-cover"
+                      src={work.image}
+                      alt={work.image_alt}
+                      width={work.image_width}
+                      height={work.image_height}
+                    />
+                  </Link>
                 </div>
 
                 <span className="w-1/12 h-0.5 bg-gray-300 mt-5 hidden md:block"></span>
@@ -113,7 +135,7 @@ const Portfolio = () => {
                   <h1 className="font-normal text-blue-500 text-3xl md:text-4xl mb-5 mt-5 md:mt-0">
                     {work.title}
                   </h1>
-                  <h6>StartUp&apos;s Directory</h6>
+                  <h6>{work.subtitle}</h6>
 
                   <p className="font-normal text-gray-500 text-sm md:text-base">
                     {work.description}
